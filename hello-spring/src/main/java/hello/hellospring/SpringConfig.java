14a608515f6ff2c703168165b297720f31cf7c71
junhwan <ujunhwan@gmail.com>
2020-12-10 11:24:28 +0900

    JdbcTemplate

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,6 +1,7 @@
 package hello.hellospring;
 
 import hello.hellospring.repository.JdbcMemberRepository;
+import hello.hellospring.repository.JdbcTemplateMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
@@ -36,7 +37,8 @@ public class SpringConfig {
         // DI 사용의 이점. 기존의 코드를 손대지 않고, Config 만으로 구현 클래스를 변경할 수 있음
         // 이 부분만 수정하면 변수 저장에서 DB 저장으로 간편하게 바꿀 수 있다
 //        return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+//        return new JdbcMemberRepository(dataSource);
+        return new JdbcTemplateMemberRepository(dataSource);
     }
 
 }
